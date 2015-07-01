f828fbe43cc202766a9737a18fd20fd165e71e15
lamogui <julien.deloor@gmail.com>
2015-07-01 17:42:57 +0200

    Begin an experiment with Agl

---
+++ include/libtools/decoders/agldecoder.hpp
@@ -0,0 +1,62 @@
+/*
+
+THIS FILE IS SUBJECT TO STRICT RULES OF BDE NE777 COPYDOWN.
+NOBODY IS PERMITTED TO COPY AND DISTRIBUTE VERBATIM OR MODIFIED COPIES OF
+THIS LICENSE HEADER. A SECURITY LEVEL IS ASSIGNED TO THE FILE AND A VISIBILITY.
+THIS FILE MUST NOT BE COPIED OR REDISTRIBUTED IF IT'S VISIBILITY IS NOT PUBLIC.
+VISIBILITY HAS 3 POSSIBLE STATES:
+- PRIVATE: LIMITED TO PROJECTS WITH THE SAME SECURITY LEVEL WITHIN THE BDE NE777. (DEFAULT VISIBILITY)
+- PROTECTED: LIMITED TO PROJECTS USING THE SAME SECURITY RULES WITHIN THE BDE NE777.
+- PUBLIC: USABLE FOR FREE IN PUBLIC PROJECTS UNDER THE FOLLOWING LICENSES: BSD, MIT OR DWTFYWT
+WITHOUT OTHER CONDITIONS THAN THE CONSERVATION OF THIS HEADER INCLUDING:
+RULES, ORIGINAL AUTHORS (WITH EMAIL), ORIGINAL FILENAME, AND VERSION, INSIDE THE FILE
+WHICH CONTAINS PART (OR ENTIRE) CODE FROM THIS FILE. USABLE IN OTHER PROJECTS WITH CONDITIONS
+AND REMUMERATIONS, FIXED BY ORIGINAL AUTHORS (CONTACT THEM).
+
+*/
+
+/**
+  * THIS FILE IS PART OF LIBTOOLS
+  * SECURITY LEVEL : 8
+  * VISIBILITY     : PROTECTED
+  * © COPYDOWN™ LAMOGUI ALL RIGHTS RESERVED
+  *
+  * FILE         : agldecoder.hpp
+  * AUTHORS      : Julien De Loor (julien.deloor@gmail.com)
+  * VERSION      : 1.0
+  */
+
+#ifndef LIBTOOLS_AGLDECODER_HPP
+#define LIBTOOLS_AGLDECODER_HPP
+
+#include <libtools/public/config.h>
+#include <libtools/core/decoder.hpp>
+#include <tinyxml2.h>
+
+class AglDecoder : public MusicDecoder
+{
+public:
+
+  AglDecoder();
+  virtual ~AglDecoder();
+
+  virtual unsigned int fetch(Signal& outleft, Signal& outright);
+
+  virtual bool ended() const;
+  virtual void rewind();
+  virtual double length() const;
+
+protected:
+  virtual bool _open(const string_t& filename);
+  virtual bool _load(const uint8_t* buffer, unsigned int size);
+  virtual void _reset();
+
+  bool _parse(); //Parse the xml
+
+
+  tinyxml2::XMLDocument _xmldoc;
+
+
+};
+
+#endif
